8308ac5725f843821ac35130771b680e491efc6b
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-04-04 21:02:16 -0900

    Style: Update
    Create emploeeys state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,20 @@ import AppInfo from "../AppInfo/AppInfo";
 import SearchPanel from "../SearchPanel/SearchPanel";
 import AppFilter from "../AppFilter/AppFilter";
 import EmployeesList from "../EmployeesList/EmployeesList";
+import { useState } from "react";
 function App() {
+  const [employees, setEmployees] = useState([
+    {
+      name: "Viktor",
+      salary: 1000,
+      isRaise: true,
+    },
+    {
+      name: "Serhii",
+      salary: 500,
+      isRaise: false,
+    },
+  ]);
   return (
     <div className={css.container}>
       <AppInfo />
