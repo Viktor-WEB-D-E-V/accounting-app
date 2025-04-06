a2ae354b0c16b7c968623eac12c857ef21ece77b
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-04-05 21:24:51 -0900

    Added markup for the form for adding a new employee

--- src/components/EmployeesAddForm/EmployeesAddForm.jsx
+++ src/components/EmployeesAddForm/EmployeesAddForm.jsx
@@ -1,6 +1,16 @@
 import css from "./EmployeesAddForm.css";
 
 const EmployeesAddForm = () => {
-  return <></>;
+  return (
+    <>
+      <h3>Add a new employee</h3>
+      <form>
+        <input type="text" placeholder="Name" />
+        <input type="number" placeholder="Salary" />
+
+        <button type="submit">Add</button>
+      </form>
+    </>
+  );
 };
 export default EmployeesAddForm;
