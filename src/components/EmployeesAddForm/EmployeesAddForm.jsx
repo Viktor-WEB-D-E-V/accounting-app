e092effc934976ce152972fdb01789351219a657
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-04-09 22:03:50 -0900

    Refactor convert form component to class-based wit internal state. Added: onValueChange for tracking input value

--- src/components/EmployeesAddForm/EmployeesAddForm.jsx
+++ src/components/EmployeesAddForm/EmployeesAddForm.jsx
@@ -1,16 +1,51 @@
 import css from "./EmployeesAddForm.module.css";
+import { Component } from "react";
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
 
-const EmployeesAddForm = () => {
-  return (
-    <div className={css.container}>
-      <h3 className={css.title}>Add a new employee</h3>
-      <form className={css.form}>
-        <input className={css.formInput} type="text" placeholder="Name" />
-        <input className={css.formInput} type="number" placeholder="Salary" />
+    this.state = {
+      name: "",
+      salary: "",
+    };
+  }
 
-        <button className={css.submitBtn} type="submit">Add</button>
-      </form>
-    </div>
-  );
-};
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+
+    return (
+      <div className={css.container}>
+        <h3 className={css.title}>Add a new employee</h3>
+        <form className={css.form}>
+          <input
+            className={css.formInput}
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={this.onValueChange}
+          />
+          <input
+            className={css.formInput}
+            type="text"
+            placeholder="Salary"
+            name="salary"
+            value={salary}
+            onChange={this.onValueChange}
+          />
+
+          <button className={css.submitBtn} type="submit">
+            Add
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 export default EmployeesAddForm;
